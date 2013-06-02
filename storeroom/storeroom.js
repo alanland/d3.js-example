26c0f33e638927b16a1a5c871f030482d54a1427
alanland <alanland@163.com>
2013-06-02 13:32:19 +0800

    增加 仓位图 示例。

---
+++ storeroom/storeroom.js
@@ -0,0 +1,869 @@
+var data = {
+    name:'仓库',
+    "children": [
+        {
+            "x": 1,
+            "y": 1,
+            "capacity": 100,
+            "used": 88
+        },
+        {
+            "x": 1,
+            "y": 2,
+            "capacity": 100,
+            "used": 41
+        },
+        {
+            "x": 1,
+            "y": 3,
+            "capacity": 100,
+            "used": 26
+        },
+        {
+            "x": 1,
+            "y": 4,
+            "capacity": 100,
+            "used": 42
+        },
+        {
+            "x": 1,
+            "y": 5,
+            "capacity": 100,
+            "used": 30
+        },
+        {
+            "x": 1,
+            "y": 6,
+            "capacity": 100,
+            "used": 96
+        },
+        {
+            "x": 1,
+            "y": 7,
+            "capacity": 100,
+            "used": 35
+        },
+        {
+            "x": 1,
+            "y": 8,
+            "capacity": 100,
+            "used": 91
+        },
+        {
+            "x": 1,
+            "y": 9,
+            "capacity": 100,
+            "used": 33
+        },
+        {
+            "x": 1,
+            "y": 10,
+            "capacity": 100,
+            "used": 27
+        },
+        {
+            "x": 1,
+            "y": 11,
+            "capacity": 100,
+            "used": 28
+        },
+        {
+            "x": 1,
+            "y": 12,
+            "capacity": 100,
+            "used": 64
+        },
+        {
+            "x": 2,
+            "y": 1,
+            "capacity": 100,
+            "used": 5
+        },
+        {
+            "x": 2,
+            "y": 2,
+            "capacity": 100,
+            "used": 89
+        },
+        {
+            "x": 2,
+            "y": 3,
+            "capacity": 100,
+            "used": 19
+        },
+        {
+            "x": 2,
+            "y": 4,
+            "capacity": 100,
+            "used": 10
+        },
+        {
+            "x": 2,
+            "y": 5,
+            "capacity": 100,
+            "used": 88
+        },
+        {
+            "x": 2,
+            "y": 6,
+            "capacity": 100,
+            "used": 94
+        },
+        {
+            "x": 2,
+            "y": 7,
+            "capacity": 100,
+            "used": 99
+        },
+        {
+            "x": 2,
+            "y": 8,
+            "capacity": 100,
+            "used": 71
+        },
+        {
+            "x": 2,
+            "y": 9,
+            "capacity": 100,
+            "used": 98
+        },
+        {
+            "x": 2,
+            "y": 10,
+            "capacity": 100,
+            "used": 34
+        },
+        {
+            "x": 2,
+            "y": 11,
+            "capacity": 100,
+            "used": 4
+        },
+        {
+            "x": 2,
+            "y": 12,
+            "capacity": 100,
+            "used": 34
+        },
+        {
+            "x": 3,
+            "y": 1,
+            "capacity": 100,
+            "used": 56
+        },
+        {
+            "x": 3,
+            "y": 2,
+            "capacity": 100,
+            "used": 16
+        },
+        {
+            "x": 3,
+            "y": 3,
+            "capacity": 100,
+            "used": 8
+        },
+        {
+            "x": 3,
+            "y": 4,
+            "capacity": 100,
+            "used": 93
+        },
+        {
+            "x": 3,
+            "y": 5,
+            "capacity": 100,
+            "used": 95
+        },
+        {
+            "x": 3,
+            "y": 6,
+            "capacity": 100,
+            "used": 26
+        },
+        {
+            "x": 3,
+            "y": 7,
+            "capacity": 100,
+            "used": 72
+        },
+        {
+            "x": 3,
+            "y": 8,
+            "capacity": 100,
+            "used": 86
+        },
+        {
+            "x": 3,
+            "y": 9,
+            "capacity": 100,
+            "used": 52
+        },
+        {
+            "x": 3,
+            "y": 10,
+            "capacity": 100,
+            "used": 58
+        },
+        {
+            "x": 3,
+            "y": 11,
+            "capacity": 100,
+            "used": 52
+        },
+        {
+            "x": 3,
+            "y": 12,
+            "capacity": 100,
+            "used": 95
+        },
+        {
+            "x": 4,
+            "y": 1,
+            "capacity": 100,
+            "used": 97
+        },
+        {
+            "x": 4,
+            "y": 2,
+            "capacity": 100,
+            "used": 14
+        },
+        {
+            "x": 4,
+            "y": 3,
+            "capacity": 100,
+            "used": 66
+        },
+        {
+            "x": 4,
+            "y": 4,
+            "capacity": 100,
+            "used": 58
+        },
+        {
+            "x": 4,
+            "y": 5,
+            "capacity": 100,
+            "used": 27
+        },
+        {
+            "x": 4,
+            "y": 6,
+            "capacity": 100,
+            "used": 58
+        },
+        {
+            "x": 4,
+            "y": 7,
+            "capacity": 100,
+            "used": 24
+        },
+        {
+            "x": 4,
+            "y": 8,
+            "capacity": 100,
+            "used": 74
+        },
+        {
+            "x": 4,
+            "y": 9,
+            "capacity": 100,
+            "used": 88
+        },
+        {
+            "x": 4,
+            "y": 10,
+            "capacity": 100,
+            "used": 99
+        },
+        {
+            "x": 4,
+            "y": 11,
+            "capacity": 100,
+            "used": 23
+        },
+        {
+            "x": 4,
+            "y": 12,
+            "capacity": 100,
+            "used": 10
+        },
+        {
+            "x": 5,
+            "y": 1,
+            "capacity": 100,
+            "used": 37
+        },
+        {
+            "x": 5,
+            "y": 2,
+            "capacity": 100,
+            "used": 5
+        },
+        {
+            "x": 5,
+            "y": 3,
+            "capacity": 100,
+            "used": 84
+        },
+        {
+            "x": 5,
+            "y": 4,
+            "capacity": 100,
+            "used": 43
+        },
+        {
+            "x": 5,
+            "y": 5,
+            "capacity": 100,
+            "used": 97
+        },
+        {
+            "x": 5,
+            "y": 6,
+            "capacity": 100,
+            "used": 27
+        },
+        {
+            "x": 5,
+            "y": 7,
+            "capacity": 100,
+            "used": 23
+        },
+        {
+            "x": 5,
+            "y": 8,
+            "capacity": 100,
+            "used": 61
+        },
+        {
+            "x": 5,
+            "y": 9,
+            "capacity": 100,
+            "used": 42
+        },
+        {
+            "x": 5,
+            "y": 10,
+            "capacity": 100,
+            "used": 48
+        },
+        {
+            "x": 5,
+            "y": 11,
+            "capacity": 100,
+            "used": 33
+        },
+        {
+            "x": 5,
+            "y": 12,
+            "capacity": 100,
+            "used": 63
+        },
+        {
+            "x": 6,
+            "y": 1,
+            "capacity": 100,
+            "used": 27
+        },
+        {
+            "x": 6,
+            "y": 2,
+            "capacity": 100,
+            "used": 83
+        },
+        {
+            "x": 6,
+            "y": 3,
+            "capacity": 100,
+            "used": 37
+        },
+        {
+            "x": 6,
+            "y": 4,
+            "capacity": 100,
+            "used": 84
+        },
+        {
+            "x": 6,
+            "y": 5,
+            "capacity": 100,
+            "used": 82
+        },
+        {
+            "x": 6,
+            "y": 6,
+            "capacity": 100,
+            "used": 18
+        },
+        {
+            "x": 6,
+            "y": 7,
+            "capacity": 100,
+            "used": 59
+        },
+        {
+            "x": 6,
+            "y": 8,
+            "capacity": 100,
+            "used": 44
+        },
+        {
+            "x": 6,
+            "y": 9,
+            "capacity": 100,
+            "used": 26
+        },
+        {
+            "x": 6,
+            "y": 10,
+            "capacity": 100,
+            "used": 40
+        },
+        {
+            "x": 6,
+            "y": 11,
+            "capacity": 100,
+            "used": 89
+        },
+        {
+            "x": 6,
+            "y": 12,
+            "capacity": 100,
+            "used": 98
+        },
+        {
+            "x": 7,
+            "y": 1,
+            "capacity": 100,
+            "used": 48
+        },
+        {
+            "x": 7,
+            "y": 2,
+            "capacity": 100,
+            "used": 60
+        },
+        {
+            "x": 7,
+            "y": 3,
+            "capacity": 100,
+            "used": 29
+        },
+        {
+            "x": 7,
+            "y": 4,
+            "capacity": 100,
+            "used": 21
+        },
+        {
+            "x": 7,
+            "y": 5,
+            "capacity": 100,
+            "used": 9
+        },
+        {
+            "x": 7,
+            "y": 6,
+            "capacity": 100,
+            "used": 99
+        },
+        {
+            "x": 7,
+            "y": 7,
+            "capacity": 100,
+            "used": 73
+        },
+        {
+            "x": 7,
+            "y": 8,
+            "capacity": 100,
+            "used": 21
+        },
+        {
+            "x": 7,
+            "y": 9,
+            "capacity": 100,
+            "used": 32
+        },
+        {
+            "x": 7,
+            "y": 10,
+            "capacity": 100,
+            "used": 89
+        },
+        {
+            "x": 7,
+            "y": 11,
+            "capacity": 100,
+            "used": 20
+        },
+        {
+            "x": 7,
+            "y": 12,
+            "capacity": 100,
+            "used": 0
+        },
+        {
+            "x": 8,
+            "y": 1,
+            "capacity": 100,
+            "used": 62
+        },
+        {
+            "x": 8,
+            "y": 2,
+            "capacity": 100,
+            "used": 36
+        },
+        {
+            "x": 8,
+            "y": 3,
+            "capacity": 100,
+            "used": 90
+        },
+        {
+            "x": 8,
+            "y": 4,
+            "capacity": 100,
+            "used": 42
+        },
+        {
+            "x": 8,
+            "y": 5,
+            "capacity": 100,
+            "used": 57
+        },
+        {
+            "x": 8,
+            "y": 6,
+            "capacity": 100,
+            "used": 39
+        },
+        {
+            "x": 8,
+            "y": 7,
+            "capacity": 100,
+            "used": 32
+        },
+        {
+            "x": 8,
+            "y": 8,
+            "capacity": 100,
+            "used": 99
+        },
+        {
+            "x": 8,
+            "y": 9,
+            "capacity": 100,
+            "used": 94
+        },
+        {
+            "x": 8,
+            "y": 10,
+            "capacity": 100,
+            "used": 35
+        },
+        {
+            "x": 8,
+            "y": 11,
+            "capacity": 100,
+            "used": 87
+        },
+        {
+            "x": 8,
+            "y": 12,
+            "capacity": 100,
+            "used": 98
+        },
+        {
+            "x": 9,
+            "y": 1,
+            "capacity": 100,
+            "used": 91
+        },
+        {
+            "x": 9,
+            "y": 2,
+            "capacity": 100,
+            "used": 59
+        },
+        {
+            "x": 9,
+            "y": 3,
+            "capacity": 100,
+            "used": 16
+        },
+        {
+            "x": 9,
+            "y": 4,
+            "capacity": 100,
+            "used": 51
+        },
+        {
+            "x": 9,
+            "y": 5,
+            "capacity": 100,
+            "used": 8
+        },
+        {
+            "x": 9,
+            "y": 6,
+            "capacity": 100,
+            "used": 77
+        },
+        {
+            "x": 9,
+            "y": 7,
+            "capacity": 100,
+            "used": 30
+        },
+        {
+            "x": 9,
+            "y": 8,
+            "capacity": 100,
+            "used": 95
+        },
+        {
+            "x": 9,
+            "y": 9,
+            "capacity": 100,
+            "used": 11
+        },
+        {
+            "x": 9,
+            "y": 10,
+            "capacity": 100,
+            "used": 65
+        },
+        {
+            "x": 9,
+            "y": 11,
+            "capacity": 100,
+            "used": 15
+        },
+        {
+            "x": 9,
+            "y": 12,
+            "capacity": 100,
+            "used": 38
+        },
+        {
+            "x": 10,
+            "y": 1,
+            "capacity": 100,
+            "used": 16
+        },
+        {
+            "x": 10,
+            "y": 2,
+            "capacity": 100,
+            "used": 14
+        },
+        {
+            "x": 10,
+            "y": 3,
+            "capacity": 100,
+            "used": 27
+        },
+        {
+            "x": 10,
+            "y": 4,
+            "capacity": 100,
+            "used": 86
+        },
+        {
+            "x": 10,
+            "y": 5,
+            "capacity": 100,
+            "used": 64
+        },
+        {
+            "x": 10,
+            "y": 6,
+            "capacity": 100,
+            "used": 75
+        },
+        {
+            "x": 10,
+            "y": 7,
+            "capacity": 100,
+            "used": 95
+        },
+        {
+            "x": 10,
+            "y": 8,
+            "capacity": 100,
+            "used": 72
+        },
+        {
+            "x": 10,
+            "y": 9,
+            "capacity": 100,
+            "used": 12
+        },
+        {
+            "x": 10,
+            "y": 10,
+            "capacity": 100,
+            "used": 40
+        },
+        {
+            "x": 10,
+            "y": 11,
+            "capacity": 100,
+            "used": 87
+        },
+        {
+            "x": 10,
+            "y": 12,
+            "capacity": 100,
+            "used": 2
+        },
+        {
+            "x": 11,
+            "y": 1,
+            "capacity": 100,
+            "used": 50
+        },
+        {
+            "x": 11,
+            "y": 2,
+            "capacity": 100,
+            "used": 59
+        },
+        {
+            "x": 11,
+            "y": 3,
+            "capacity": 100,
+            "used": 19
+        },
+        {
+            "x": 11,
+            "y": 4,
+            "capacity": 100,
+            "used": 36
+        },
+        {
+            "x": 11,
+            "y": 5,
+            "capacity": 100,
+            "used": 75
+        },
+        {
+            "x": 11,
+            "y": 6,
+            "capacity": 100,
+            "used": 56
+        },
+        {
+            "x": 11,
+            "y": 7,
+            "capacity": 100,
+            "used": 87
+        },
+        {
+            "x": 11,
+            "y": 8,
+            "capacity": 100,
+            "used": 8
+        },
+        {
+            "x": 11,
+            "y": 9,
+            "capacity": 100,
+            "used": 17
+        },
+        {
+            "x": 11,
+            "y": 10,
+            "capacity": 100,
+            "used": 4
+        },
+        {
+            "x": 11,
+            "y": 11,
+            "capacity": 100,
+            "used": 97
+        },
+        {
+            "x": 11,
+            "y": 12,
+            "capacity": 100,
+            "used": 67
+        },
+        {
+            "x": 12,
+            "y": 1,
+            "capacity": 100,
+            "used": 81
+        },
+        {
+            "x": 12,
+            "y": 2,
+            "capacity": 100,
+            "used": 68
+        },
+        {
+            "x": 12,
+            "y": 3,
+            "capacity": 100,
+            "used": 79
+        },
+        {
+            "x": 12,
+            "y": 4,
+            "capacity": 100,
+            "used": 14
+        },
+        {
+            "x": 12,
+            "y": 5,
+            "capacity": 100,
+            "used": 39
+        },
+        {
+            "x": 12,
+            "y": 6,
+            "capacity": 100,
+            "used": 68
+        },
+        {
+            "x": 12,
+            "y": 7,
+            "capacity": 100,
+            "used": 100
+        },
+        {
+            "x": 12,
+            "y": 8,
+            "capacity": 100,
+            "used": 28
+        },
+        {
+            "x": 12,
+            "y": 9,
+            "capacity": 100,
+            "used": 33
+        },
+        {
+            "x": 12,
+            "y": 10,
+            "capacity": 100,
+            "used": 87
+        },
+        {
+            "x": 12,
+            "y": 11,
+            "capacity": 100,
+            "used": 54
+        },
+        {
+            "x": 12,
+            "y": 12,
+            "capacity": 100,
+            "used": 75
+        }
+    ]
+}
